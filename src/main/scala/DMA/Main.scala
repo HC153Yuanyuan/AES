b9e11afbb8e3bba728f7f80eefa5d5a0537ae4f1
zhangyuanyuan <1060223758@qq.com>
2022-08-01 09:11:14 +0800

    add dma tester

--- src/main/scala/DMA/Main.scala
+++ src/main/scala/DMA/Main.scala
@@ -3,12 +3,14 @@ package DMA
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba3.ahblite.AhbLite3Config
 
 
 object Main extends App{
   val cfg = new MacDmaConfig(addressWidth = 32, dataWidth = 32, enableRx = true, enableTx = false,burstCountWidth = 4)
   val dmaCfg = DmaCfg(slaveNode = 8)
+  val ahbCfg = AhbLite3Config(addressWidth = dmaCfg.addrWidth,dataWidth = dmaCfg.dataWidth)
 
-  SpinalConfig(targetDirectory = "rtl").generateVerilog(DmaArbiter(dmaCfg))
+  SpinalConfig(targetDirectory = "rtl").generateVerilog(DmaCtrl(dmaCfg,ahbCfg))
 }
 
